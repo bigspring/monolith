091d9e9044e9511627f3fb6767831ab41cbad289
jrgmartin <jon@bigspring.co.uk>
2012-09-14 18:03:27 +0200

    Update category.php

--- category.php
+++ category.php
@@ -13,7 +13,7 @@
 					<li>
 						<article>
 							<h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-							<time datetime="<?php the_time( 'Y-m-D' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
+							<time datetime="<?php the_time( 'Y-m-D' ); ?>" pubdate="<?php the_date(); ?> <?php the_time(); ?><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
 							<?php the_content(); ?>
 						</article>
 					</li>
