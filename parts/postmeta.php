6b1d43cf7feb00db669a57b7d58979c1718c56ba
jrgmartin <jon@bigspring.co.uk>
2012-09-14 18:03:58 +0200

    Update parts/postmeta.php

--- parts/postmeta.php
+++ parts/postmeta.php
@@ -1,1 +1,1 @@
-<time datetime="<?php the_time( 'Y-m-D' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time>&nbsp;&nbsp;|&nbsp;&nbsp;<?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
+<time datetime="<?php the_time( 'Y-m-D' ); ?>" pubdate="<?php the_date(); ?> <?php the_time(); ?>><?php the_date(); ?> <?php the_time(); ?></time>&nbsp;&nbsp;|&nbsp;&nbsp;<?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
