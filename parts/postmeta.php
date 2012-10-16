a54d80b03ba8ba2f8833e6e5cb3ecd6baaf14df0
simonpmiles <simon@bigspring.co.uk>
2012-10-16 12:24:29 +0100

    Changed postmeta so it displays date and not just time

--- parts/postmeta.php
+++ parts/postmeta.php
@@ -1,1 +1,1 @@
-<time datetime="<?php the_time( 'Y-m-D' ); ?>" pubdate="<?php the_date(); ?> <?php the_time(); ?>"><?php the_date(); ?> <?php the_time(); ?></time>&nbsp;&nbsp;|&nbsp;&nbsp;<?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
+<?php the_date('D-m-Y'); ?>&nbsp;&nbsp;<?php the_time(); ?> &nbsp;&nbsp;|&nbsp;&nbsp;<?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
