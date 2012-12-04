ef5512f7715cdc49560a3488453ad19350232428
simonpmiles <simon@bigspring.co.uk>
2012-12-04 15:17:20 +0000

    additions to monolith
    added glyph icons for CPTs
    added childpages shortcodes
    added columns shortcode and css rule

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -207,6 +207,276 @@ function blockquotes( $atts, $content = null ) {
 	return $output;
 }
 
+/**
+ * childpages shortcode renders a bootstrap thumbnail ul of childpages including post thumbnail. title and read more button.
+ * @param array $atts
+ * @param string $content
+ */
+function childpages($atts, $content = null)
+{
+	global $post;
+
+	if(is_array($atts) && array_key_exists('id', $atts))
+		$id = $atts['id'];
+	else
+		$id = $post->ID;
+
+	$args = array(
+			'post_parent' => $post->ID,
+			'post_type' => 'page'
+	);
+
+	// get all attachments
+	$childpages = get_children($args);
+	
+
+
+	$html = '<ul class="thumbnails">';
+
+	foreach ($childpages AS $childpage)
+	{
+			$html .= '<li class="span2">';
+			$html .= get_the_post_thumbnail($childpage->ID);
+			$html .= $childpage->post_title;
+			$html .= '<a href="'. get_permalink($childpage->ID) .' " class="btn">More</a>';
+			$html .= '</li>';
+	}
+
+	$html .= '</ul>';
+
+	return $html;
+}
+
+
+/**
+ * sub_tabs_collapse shortcode, renders a bootstrap tabs of child pages with an accordion within of grandchildren pages
+ * @param array $atts
+ * @param string $content
+ */
+function sub_tabs_collapse($atts, $content = null)
+{
+	global $post;
+
+	if(is_array($atts) && array_key_exists('id', $atts))
+		$id = $atts['id'];
+	else
+		$id = $post->ID;
+
+	$args = array(
+			'post_parent' => $post->ID,
+			'post_type' => 'page',
+			'order' => 'ASC',
+			'orderby' => 'menu_order'
+	);
+
+	// get all attachments
+	$childpages = get_children($args);
+	
+	$count = 0;
+
+	$html = '<div class="child-tabs">';
+	$html .= '<ul class="nav nav-tabs">';
+
+	foreach ($childpages AS $childpage)
+	{
+			$active = '';
+			if ($count == 0) $active = 'class="active"';
+			
+			$html .= '<li '. $active . '>';
+			$html .= '<a href="#' . $childpage->ID . '" data-toggle="tab">' . $childpage->post_title . '</a>';
+			$html .= '</li>';
+			
+			$count++;
+	}
+
+	$html .= '</ul>';
+	
+	$count = 0;
+
+	$html .= '<div class="tab-content">';
+	
+	foreach ($childpages AS $childpage)
+	{
+			$active = '';
+			if ($count == 0) $active = 'active';
+
+			$html .= '<div class="tab-pane ' . $active . '" id="' . $childpage->ID .'">';
+			
+			$args = array(
+				'post_parent' => $childpage->ID,
+				'post_type' => 'page',
+				'order' => 'ASC',
+				'orderby' => 'menu_order'
+			);
+
+			// get all attachments
+			$grandchildpages = get_children($args);
+			
+			$html .= '<div class="accordion" id="accordion">';
+			
+			foreach($grandchildpages AS $grandchildpage)
+			{
+
+				$html .= '<div class="accordion-group">';
+				$html .= '<div class="accordion-heading">';
+				$html .= '<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse'.$grandchildpage->ID.'">';
+				$html .= $grandchildpage->post_title;
+				$html .= '</a>';
+				$html .= '</div>';
+				$html .= '<div id="collapse'.$grandchildpage->ID.'" class="accordion-body collapse">';
+				$html .= '<div class="accordion-inner">';
+				$html .= apply_filters('the_content', $grandchildpage->post_content);
+				$html .= '</div>';
+				$html .= '</div>';
+				$html .= '</div>';
+
+			}
+			
+			$html .= '</div>';
+			$html .= '</div>';
+			
+			$count++;
+	}
+
+	$html .= '</div>';
+	$html .= '</div>';
+	return $html;
+
+
+}
+
+//================================ end sub tabs accordion shortcode ====================================
+
+/**
+ * sub_tabs shortcode renders bootstrap tabs of child pages
+ * @param array $atts
+ * @param string $content
+ */
+function sub_tabs($atts, $content = null)
+{
+	global $post;
+
+	if(is_array($atts) && array_key_exists('id', $atts))
+		$id = $atts['id'];
+	else
+		$id = $post->ID;
+
+	$args = array(
+			'post_parent' => $post->ID,
+			'post_type' => 'page',
+			'order' => 'ASC',
+			'orderby' => 'menu_order'
+	);
+
+	// get all attachments
+	$childpages = get_children($args);
+	
+	$count = 0;
+
+	$html = '<div class="child-tabs">';
+	$html .= '<ul class="nav nav-tabs">';
+
+	foreach ($childpages AS $childpage)
+	{
+			$active = '';
+			if ($count == 0) $active = 'class="active"';
+			
+			$html .= '<li '. $active . '>';
+			$html .= '<a href="#' . $childpage->ID . '" data-toggle="tab">' . $childpage->post_title . '</a>';
+			$html .= '</li>';
+			
+			$count++;
+	}
+
+	$html .= '</ul>';
+	
+	$count = 0;
+
+	$html .= '<div class="tab-content">';
+	
+	foreach ($childpages AS $childpage)
+	{
+			$active = '';
+			if ($count == 0) $active = 'active';
+
+			$html .= '<div class="tab-pane ' . $active . '" id="' . $childpage->ID .'">';
+			
+			if($childpage->post_title == 'Specification Sheets') // if it's a spec sheet page, get the documents
+			{
+				$html .= do_shortcode('[documents_modified]');
+			}
+			else
+			{
+				$html .= apply_filters('the_content', $childpage->post_content);
+			}
+
+			$html .= '</div>';
+			
+			$count++;
+	}
+
+	$html .= '</div>';
+	$html .= '</div>';
+	return $html;
+
+
+}
+
+//================================ end sub tabs shortcode ====================================
+
+/**
+ * childpageslist shortcode renders a list of childpages
+ * @param array $atts
+ * @param string $content
+ */
+function childpageslist($atts, $content = null)
+{
+	global $post;
+
+	if(is_array($atts) && array_key_exists('id', $atts))
+		$id = $atts['id'];
+	else
+		$id = $post->ID;
+
+	$args = array(
+			'post_parent' => $post->ID,
+			'post_type' => 'page',
+			'order' => 'ASC',
+			'orderby' => 'menu_order'
+
+	);
+
+	// get all attachments
+	$childpages = get_children($args);
+	
+
+
+	$html = '<ul class="unstyled childpages">';
+
+	foreach ($childpages AS $childpage)
+	{
+			$html .= '<li>';
+			$html .= '<a href="'. get_permalink($childpage->ID) . '">' . $childpage->post_title . '</a>';
+			$html .= '</li>';
+	}
+
+	$html .= '</ul>';
+
+	return $html;
+}
+
+//================================ end childpage list shortcode ====================================
+
+//shortcode for bio column structures
+function columns_shortcode($atts, $content = null) {
+		global $post;
+		
+		$excerpt = $post->post_excerpt;
+		
+		return '<div class="columns">' . do_shortcode($content) . '</div>';
+}
+
+
 // load shortcodes
 add_shortcode('icon', 'bootstrap_glyph_icons');
 add_shortcode('contact', 'contact_us_shortcode');
@@ -217,4 +487,10 @@ add_shortcode('button', 'buttons');
 add_shortcode('alert', 'alerts');
 add_shortcode('block-message', 'block_messages');
 add_shortcode('blockquote', 'blockquotes');
+add_shortcode('childpages', 'childpages');
+add_shortcode('childpageslist', 'childpageslist');
+add_shortcode('sub_tabs', 'sub_tabs');
+add_shortcode('sub_tabs_collapse', 'sub_tabs_collapse'); 
+add_shortcode('columns', 'columns_shortcode');
+
 ?>
