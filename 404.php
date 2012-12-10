e65603afe1cfc8a61d4fb9c90709eba76425da7b
simonpmiles <simon@bigspring.co.uk>
2012-12-10 14:14:42 +0000

    Updated 404page
    The 404 page had no bootstrap markup so I've added this.

--- 404.php
+++ 404.php
@@ -1,6 +1,15 @@
 <?php // 404 page ?>
-<?php get_template_parts( array( 'parts/html-header', 'parts/header' ) ); ?>
-
-<h2>Page not found</h2>
-
+<? get_template_parts( array( 'parts/html-header', 'parts/header' ) ); ?>
+<div id="main">
+	<div class="<?= CONTAINER_CLASSES; ?>">
+	<? get_template_part('parts/breadcrumb'); // el loado sidebaro ?>
+		<div class="<?= ROW_CLASSES ?>">
+			<? get_template_part('parts/sidebar-left'); // el loado left sidebaro ?>			
+			<div class="span<?= MAIN_SIZE ?>">
+				<h2>Page not found</h2>
+			</div><!-- /MAIN_SIZES -->
+			<? get_template_part('parts/sidebar-right'); // el loado right sidebaro ?>
+		</div><!-- /ROW_CLASSES -->	
+	</div><!-- /CONTAINER_CLASSES -->
+</div><!-- /main -->
 <?php get_template_parts( array( 'parts/footer','parts/html-footer' ) ); ?>
