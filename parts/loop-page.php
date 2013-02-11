73cd502a6a97cb5fd65a2a81a7052185c100f791
Julio Taylor <julio@bigspring.co.uk>
2013-02-11 15:15:58 +0000

    created new loops for pages and single posts

--- parts/loop-page.php
+++ parts/loop-page.php
@@ -1,13 +1,15 @@
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 	<article>
-		<header>
-			<div class="page-header">
-				<h1><?php the_title(); ?></h1>
-			</div>
+		<header class="page-header">
+			<h1><?php the_title(); ?></h1>
 		</header>
+		
+		<?php if ($post->post_excerpt != '') { // show the excerpt if it exists ?>
+			<div class="lead">
+				<?php the_excerpt(); ?>
+			</div>
+		<? } ?>
+		
 		<?php the_content(); ?>					
-		<footer>
-			<?php // comments_template( '', true ); ?>
-		</footer>
 	</article>
 <?php endwhile; ?>
--- parts/loop-post.php
+++ parts/loop-post.php
@@ -1,11 +1,19 @@
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 <article <?php post_class(); ?>>	
-	<header>
-		<h2><?php the_title(); ?></h2>
+	<header class="page-header">
+		<h1><?php the_title(); ?></h1>
 		<?php get_template_part('parts/meta/date'); ?>
 	</header>
+
+		<?php if ($post->post_excerpt != '') { // show the excerpt if it exists ?>
+			<div class="lead">
+				<?php the_excerpt(); ?>
+			</div>
+		<? } ?>
+
 		<?php the_content(); ?>			
 	<footer>
+	
 	<?php if ( get_the_author_meta( 'description' ) ) : ?>
 		<div class="well">
 			<?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?>
