125a5b34374c418b8c97761bb36191b388f21500
Julio Taylor <julio@bigspring.co.uk>
2013-02-11 15:15:30 +0000

    cleaned up the front page loops

--- front-page.php
+++ front-page.php
@@ -18,19 +18,7 @@
 		<div class="<?= ROW_CLASSES ?>">
 			<? get_template_part('parts/sidebar-left'); // el loado sidebaro ?>
 			<div class="span<?= MAIN_SIZE ?>">
-				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-					<article>
-						<header>
-							<div class="page-header">
-								<h1><?php the_title(); ?></h1>
-							</div>
-						</header>
-						<?php the_content(); ?>	
-						<footer>
-							<?php // comments_template( '', true ); ?>
-						</footer>
-					</article>
-					<?php endwhile; ?>
+				<? get_template_part('parts/loop-posts'); // load the posts loop ?>
 			</div><? //span8 ?>	
 			<? get_template_part('parts/sidebar-right'); // el loado sidebaro ?>
 		</div><? //row ?>	
