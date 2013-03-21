5a7925853bf2d8ff9b7ee30c86914b34729fe298
Simon Miles <simon@bigspring.co.uk>
2013-03-21 11:34:27 +0000

    changed loop used on page-full.php
    changed from loop to loop-page

--- page-full.php
+++ page-full.php
@@ -9,7 +9,7 @@ Template Name: Full Width
 	<? get_template_part('parts/breadcrumb'); // el loado breadcrumbo ?>
 		<div class="<?= ROW_CLASSES ?>">
 			<div class="span<?= FULLWIDTH_SIZE ?>">
-				<? get_template_part('parts/loop'); // loado el loopo ?>
+				<? get_template_part('parts/loop-page'); // loado el loopo ?>
 			</div><!-- /MAIN_SIZES -->
 		</div><!-- /ROW_CLASSES -->	
 	</div><!-- /CONTAINER_CLASSES -->
