3d95472aea6ea51fa59c8462ffd178dcb601f534
Simon Miles <simon@bigspring.co.uk>
2013-04-09 15:57:51 +0100

    Moved thumbnail captions out of functions into inc/functions

--- inc/functions.php
+++ inc/functions.php
@@ -322,3 +322,15 @@ function login_css() {
 }
 add_action('login_head', 'login_css');
 
+
+//all post thumbnail captions
+function the_post_thumbnail_caption() {
+  global $post;
+
+  $thumbnail_id    = get_post_thumbnail_id($post->ID);
+  $thumbnail_image = get_posts(array('p' => $thumbnail_id, 'post_type' => 'attachment'));
+
+  if ($thumbnail_image && isset($thumbnail_image[0])) {
+    echo '<span>'.$thumbnail_image[0]->post_excerpt.'</span>';
+  }
+}
