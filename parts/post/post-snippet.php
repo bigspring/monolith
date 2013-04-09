25a0099ddaf3c7dea2ce663636d56aa822b9b944
Julio Taylor <julio@bigspring.co.uk>
2013-04-09 17:26:39 +0100

    minor improvements to post snippet

--- parts/post/post-snippet.php
+++ parts/post/post-snippet.php
@@ -4,6 +4,7 @@
 		<h2><a href="<?= get_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 		<?php get_template_part('parts/meta/date-authorlink'); ?>
 	</header>
-		<?php the_excerpt(); ?>
-		<?php get_template_part('parts/meta/readmore'); ?>&nbsp;<?php get_template_part('parts/meta/comments'); ?>		
+		<?php the_excerpt(); ?>		
+		<?php get_template_part('parts/meta/readmore'); ?>
 </article>
+<hr/>
