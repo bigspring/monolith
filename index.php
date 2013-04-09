2d22f8f1f3fe8f9860ce514f19a08e3f97bc1f3d
Simon Miles <simon@bigspring.co.uk>
2013-04-09 15:58:01 +0100

    Added page-header class

--- index.php
+++ index.php
@@ -3,27 +3,19 @@
 <?php get_template_parts( array( 'parts/html-header', 'parts/header' ) ); // load the header part ?>
 
 <div id="main">
-
 	<div class="<?= CONTAINER_CLASSES; ?>">
-
 		<?php get_template_part('parts/breadcrumb'); // load the breadcrumb part ?>
-
 		<div class="<?= ROW_CLASSES ?>">
-			
 			<? get_template_part('parts/sidebar-left'); // load the left sidebar ?>			
-			
-			
 			<div class="span<?= MAIN_SIZE ?>">
+				<header class="page-header">
+					<h1>The Blog</h1>
+				</header>
 				<? get_template_part('parts/loop-posts'); // load the posts loop ?>
 			</div>
-			
-			
 			<? get_template_part('parts/sidebar-right'); // load the right sidebar ?>
-
 		</div> <? // end row ?>
-
 	</div> <? // end conteiner ?>
-
 </div> <? // end #main ?>
 
 <?php get_template_parts( array( 'parts/footer','parts/html-footer') ); // load the footer part ?>
