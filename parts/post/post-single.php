d517091b7d9b721821f2a8e1661b86ef65f6a9b8
Julio Taylor <julio@bigspring.co.uk>
2013-04-12 16:56:49 +0100

    added semantic markup and schema to post and single

--- parts/post/post-single.php
+++ parts/post/post-single.php
@@ -1,26 +1,35 @@
 <article <?php post_class(); ?>>	
 	<header class="page-header">
-		<h1><?php the_title(); ?></h1>
+		<h1 class="post-title" itemprop="name"><?php the_title(); ?></h1>
 		<?php get_template_part('parts/meta/date-authorlink'); ?>
 	</header>
-
-		<?php if ($post->post_excerpt != '') { // show the excerpt if it exists ?>
-			<div class="lead">
-				<?php the_excerpt(); ?>
-			</div>
-		<? } ?>
-
-		<?php the_content(); ?>			
-	<footer>
 	
-	<?php if ( get_the_author_meta( 'description' ) ) : ?>
-		<div class="well">
-			<?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?>
-			<h3>About <?php echo get_the_author() ; ?></h3>
-			<?php the_author_meta( 'description' ); ?>
-		</div>
-	<?php endif; ?>
-	<?php comments_template( '', true ); ?>
+	<?php if ($post->post_excerpt != '') { // show the excerpt if it exists ?>
+		<section class="post-excerpt lead" itemprop="description">
+			<?php the_excerpt(); ?>
+		</section>
+	<? } ?>
+	
+	<section class="post-content" itemprop="description">
+		<?php the_content(); ?>
+	</section>
+		
+	<footer class="post-footer">
+	
+		<?php if ( get_the_author_meta( 'description' ) ) : ?>
+			<hr/>
+			<div class="media">								
+				<div class="author-image pull-left" itemprop="image"><?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?></div>
+				<div class="media-body">
+					<h3 class="author-name" itemprop="name">About <?php echo get_the_author() ; ?></h3>
+					<div class="author-description" itemprop="description"><?php the_author_meta( 'description' ); ?></div>
+				</div>
+			</div>
+		<?php endif; ?>
+		
+		<hr/>
+		<?php comments_template( '', true ); ?>
 
 	</footer>	
+
 </article>
--- parts/post/post-snippet.php
+++ parts/post/post-snippet.php
@@ -1,14 +1,19 @@
 <? // markup for post snippet, used in loops and queries ?>
 <article <?php post_class(); ?>>
+
 	<header>
-		<h2><a href="<?= get_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+		<h2 itemprop="name"><a href="<?= get_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 		<?php get_template_part('parts/meta/date-authorlink'); ?>
 	</header>
-	<section class="entry-content">
-		<?php the_excerpt(); ?>				
+	
+	<section class="entry-content" itemprop="description">
+		<?php the_excerpt(); ?>		
 	</section>	
+	
 	<footer class="article-footer">
 		<?php get_template_part('parts/meta/readmore'); ?>
 	</footer> <!-- end article footer -->
+
 </article>
+
 <hr/>
