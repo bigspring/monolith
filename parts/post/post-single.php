62bfc8ac8363d6e88b61f5af3430906fb303e908
Julio Taylor <julio@bigspring.co.uk>
2013-04-15 11:22:51 +0100

    split author snippet into its own part

--- parts/post/post-single.php
+++ parts/post/post-single.php
@@ -1,36 +1,29 @@
 <article itemscope itemtype="http://schema.org/Article" <?php post_class(); ?>>	
+	
 	<header class="page-header">
-		<h1 class="post-title" itemprop="headline"><?php the_title(); ?></h1>
+		
+		<h1 class="post-title" itemprop="headline"><?php the_title(); ?></h1>		
 		<?php get_template_part('parts/meta/date-authorlink'); ?>
+	
 	</header>
 	
 	<?php if ($post->post_excerpt != '') { // show the excerpt if it exists ?>
 		<section class="post-excerpt lead" itemprop="description">
+		
 			<?php the_excerpt(); ?>
+		
 		</section>
 	<? } ?>
 	
 	<section class="post-content" itemprop="articleBody">
+	
 		<?php the_content(); ?>
+	
 	</section>
 		
 	<footer class="post-footer">
 	
-		<?php if ( get_the_author_meta( 'description' ) ) : ?>
-			<hr/>
-			<div class="media">								
-				
-				<div class="author-image pull-left" itemprop="image"><?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?></div>
-				
-				<div class="media-body">
-					<h3 class="author-name" itemprop="name">About <?php echo get_the_author() ; ?></h3>
-					<div class="author-description" itemprop="description"><?php the_author_meta( 'description' ); ?></div>
-				</div>
-			
-			</div>
-		<?php endif; ?>
-		
-		<hr/>
+		<? get_template_part('parts/author-snippet') // load the author snippet part ?>		
 		<?php comments_template( '', true ); ?>
 
 	</footer>	
