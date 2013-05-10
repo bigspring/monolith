af0e312e9f84079c05a9a137a76db85707788564
Julio Taylor <julio@bigspring.co.uk>
2013-05-10 18:00:52 +0100

    refactored posts

--- parts/post/post-single.php
+++ parts/post/post-single.php
@@ -1,31 +1,23 @@
 <article itemscope itemtype="http://schema.org/Article" <?php post_class(); ?>>	
 	
-	<header class="page-header">
-		
+	<header class="page-header">		
 		<h1 class="post-title" itemprop="headline"><?php the_title(); ?></h1>		
-		<?php get_template_part('parts/meta/date-authorlink'); ?>
-	
+		<?php get_template_part('parts/meta/date-authorlink'); ?>	
 	</header>
 	
 	<?php if ($post->post_excerpt != '') { // show the excerpt if it exists ?>
-		<section class="post-excerpt lead" itemprop="description">
-		
-			<?php the_excerpt(); ?>
-		
-		</section>
+	<section class="post-excerpt lead" itemprop="description">		
+		<?php the_excerpt(); ?>		
+	</section>
 	<? } ?>
 	
-	<section class="post-content" itemprop="articleBody">
-	
-		<?php the_content(); ?>
-	
+	<section class="post-content" itemprop="articleBody">	
+		<?php the_content(); ?>	
 	</section>
 		
-	<footer class="post-footer">
-	
+	<footer class="post-footer">	
 		<? get_template_part('parts/author-snippet') // load the author snippet part ?>		
 		<?php comments_template( '', true ); ?>
-
 	</footer>	
 
 </article>
