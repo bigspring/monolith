6db38ac7eb0d9a8d7d6e4e53dae7b0937665160b
Julio Taylor <julio@bigspring.co.uk>
2013-05-23 13:12:12 +0100

    removed redundant views

--- 404.php
+++ 404.php
@@ -1,34 +1,39 @@
 <?php // 404 page ?>
+
+<?php // Default Page Template ?>
+
 <? get_template_parts( array( 'parts/html-header', 'parts/header' ) ); ?>
 
-<div id="main">
-
+<div class="main" role="document">
+	
 	<div class="<?= CONTAINER_CLASSES; ?>">
 
-	<? get_template_part('parts/breadcrumb'); // load the bradcrumb ?>
+		<? get_template_part('parts/breadcrumb'); // load the breadcrumbs ?>
 
 		<div class="<?= ROW_CLASSES ?>">
-
-			<? get_template_part('parts/sidebar-left'); // load the left sidebar ?>			
-
-			<div class="span<?= MAIN_SIZE ?>">
+			
+			<? get_template_part('parts/sidebar-left'); // load the left sidebar ?>		
+			
+			<div class="span<?= FULLWIDTH_SIZE ?> content" role="main">			
+			
 				<header class="page-header 404-header">
 					<h1 class="page-title h1">404 error - page not found</h1>
 				</header>
-				
+
 				<section class="entry-content">
-					<p class="entry-content">The page you're looking wasn't found. Try searching for the page you're looking for:</p>
+					<p class="entry-content">The page you're looking wasn't found. Try searching instead:</p>
 				</section>
+				
 				<!-- search form -->				
+				<section class="404-search-form">
 					<form id="searchform-404" class="blog-search form-search" method="get" action="<?php bloginfo('home') ?>">	
 							<input id="s-404" name="s" class="text" type="text" value="<?php the_search_query() ?>" size="40" />
 							<button class="btn" type="submit">Search <? bloginfo('name') ?></button>					
-					</form>				
+					</form>
+				</section>
 
-			</div><!-- /MAIN_SIZES -->
-
-			<? get_template_part('parts/sidebar-right'); // load the right sidebar ?>
-
+			</div><!-- /main -->
+				
 		</div><!-- /ROW_CLASSES -->	
 
 	</div><!-- /CONTAINER_CLASSES -->
--- parts/sidebar-left.php
+++ parts/sidebar-left.php
@@ -1,5 +1,5 @@
 <? if(ENABLE_LEFT_SIDEBAR) { // if we enabled the right sidebar, render it ?>
-	<aside id="sidebar-left" class="span<?= SIDEBAR_LEFT_SIZE?>">
+	<aside class="sidebar-left span<?= SIDEBAR_LEFT_SIZE?>" role="complementary">
 		<?php dynamic_sidebar('sidebar-left'); ?>
 	</aside>
 <? } ?>
