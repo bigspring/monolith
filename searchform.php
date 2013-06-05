0167d2ac5373d2024d27d870451d3ee951f3cbf9
Julio Taylor <julio@bigspring.co.uk>
2013-06-05 15:18:35 +0100

    refactored search form

--- searchform.php
+++ searchform.php
@@ -1,4 +1,6 @@
-<form role="search" class="form-search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <input class="input-medium search-query" type="text" value="" placeholder="Search for" name="s" id="s" />
-    <button class="btn" type="submit" id="searchsubmit" value="Search">Search</button>
+<div class="well">
+<form role="search" class="form-inline search-form" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+    <input class="search-query" type="text" value="" placeholder="Search..." name="s" id="s" />
+    <button class="btn btn-default" type="submit" id="searchsubmit" value="Search">Search</button>
 </form>
+</div>
