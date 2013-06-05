3145404663102e1db258d96e5ca30ca8e25100e9
Julio Taylor <julio@bigspring.co.uk>
2013-06-05 17:22:43 +0100

    updated js, added glyph icons

--- parts/html-footer.php
+++ parts/html-footer.php
@@ -7,18 +7,17 @@
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/holder.js"></script>
 
     <!-- bootstrap js -->
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/bootstrap-transition.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/bootstrap-alert.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/bootstrap-modal.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/bootstrap-dropdown.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/bootstrap-scrollspy.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/bootstrap-tab.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/bootstrap-tooltip.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/bootstrap-popover.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/bootstrap-button.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/bootstrap-collapse.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/bootstrap-carousel.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/bootstrap-typeahead.js"></script>
-
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/affix.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/alert.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/button.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/carousel.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/collapse.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/dropdown.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/modal.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/popover.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/scrollspy.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/tab.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/tooltip.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap/transition.js"></script>
 	</body>
 </html>
