644fbe5f5133313456af17b08e99bba8a36e13dc
Julio Taylor <julio@bigspring.co.uk>
2013-06-05 15:33:12 +0100

    improved search form

--- searchform.php
+++ searchform.php
@@ -1,6 +1,8 @@
-<div class="well">
 <form role="search" class="form-inline search-form" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <input class="search-query" type="text" value="" placeholder="Search..." name="s" id="s" />
-    <button class="btn btn-default" type="submit" id="searchsubmit" value="Search">Search</button>
+	<div class="input-group">
+	  <input class="search-query" type="search" value="" placeholder="Search..." name="s" id="s" />
+	  <span class="input-group-btn">   
+	    <button class="btn btn-default" type="submit" id="searchsubmit" value="Search">Search</button>
+	  </span>
+	</div>
 </form>
-</div>
