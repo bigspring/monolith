d6d0ee460a3415cdf09222c12701de1e34ae4590
Julio Taylor <julio@bigspring.co.uk>
2013-07-29 15:05:03 +0100

    added modernizr

--- parts/html-footer.php
+++ parts/html-footer.php
@@ -3,6 +3,7 @@
 
 	<!-- load los javascript files -->
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/holder.js"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/modernizr.js"></script>
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/css_browser_selector.js"></script>
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/holder.js"></script>
 
