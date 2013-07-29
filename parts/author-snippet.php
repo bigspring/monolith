2088f3c82f1e4e1c3f5b69f00b39f576522bca54
Julio Taylor <julio@bigspring.co.uk>
2013-07-29 13:57:14 +0100

    refactored author snippet to use panels

--- parts/author-snippet.php
+++ parts/author-snippet.php
@@ -7,15 +7,20 @@
 
 <?php if ( get_the_author_meta( 'description' ) ) : ?>
 
-	<div itemscope itemtype="http://www.schema.org/Person" class="article-author well">								
-		
-		<div class="author-image pull-right" itemprop="image"><?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?></div>
-		
-		<div class="media-body">
-			<h3 class="author-snippet-name" itemprop="author">About <?php the_author() ?></h3>
-			<p class="author-snippet-description" itemprop="description"><?php the_author_meta( 'description' ); ?></p>
-			<p class="author-snippet-link"><small>More posts by <?= the_author_posts_link(); ?> &rarr;</small></p>
+	<div itemscope itemtype="http://www.schema.org/Person" class="article-author panel">
+				
+		<div class="panel-heading">
+			<h3 class="author-snippet-name panel-title" itemprop="author">About the author</h3>
 		</div>
+		
+		<div class="panel-content">
+			<div class="author-image pull-right" itemprop="image"><?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?></div>
+
+			<h3><?php the_author() ?></h3>
+			<p class="author-snippet-description text-muted" itemprop="description"><?php the_author_meta( 'description' ); ?></p>
+			<p class="author-snippet-link"><small>More posts by <?= the_author_posts_link(); ?> &rarr;</small></p>
+			<div class="clearfix"></div>
+		</div>		
 	
 	</div>
 
