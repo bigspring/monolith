8cd840c63bd8c74be4a1ea21733320c4e96567e7
Simon Miles <simon@bigspring.co.uk>
2013-07-29 16:53:27 +0100

    Searchform widget
    Fixed and applied BS3 markup

--- searchform.php
+++ searchform.php
@@ -1,8 +1,12 @@
 <form role="search" class="form-inline search-form" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-	<div class="input-group">
-	  <input class="search-query" type="search" value="" placeholder="Search..." name="s" id="s" />
-	  <span class="input-group-btn">   
-	    <button class="btn btn-default" type="submit" id="searchsubmit" value="Search"><span class="glyphicon glyphicon-search"></span></button>
-	  </span>
+	<div class="row">
+	  <div class="col-lg-12">
+	    <div class="input-group">
+			<input class="form-control" type="search" value="" placeholder="Search..." name="s" id="s" />
+			<span class="input-group-btn">
+				<button class="btn btn-default" type="submit" id="searchsubmit" value="Search"><i class="icon icon-search"></i></button>	     
+			</span>
+	    </div><!-- /input-group -->
+	  </div><!-- /.col-lg-6 -->
 	</div>
 </form>
