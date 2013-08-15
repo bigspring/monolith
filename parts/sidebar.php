abee399d5c70d6e5b91d92ed909f1fb584e0257f
Julio Taylor <julio@bigspring.co.uk>
2013-08-15 13:48:25 +0100

    minor amend to sidebar, bumped version

--- parts/sidebar.php
+++ parts/sidebar.php
@@ -1,3 +1,3 @@
-<aside class="sidebar-right <?= SIDEBAR_SIZE?>" role="complementary">
+<aside class="sidebar <?= SIDEBAR_SIZE?>" role="complementary">
 	<?php dynamic_sidebar('sidebar-right'); ?>
 </aside> 
