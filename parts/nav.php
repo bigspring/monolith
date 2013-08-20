e7a1f9814df2807fd4da0a26c8c77071d943fc57
Simon Miles <simon@bigspring.co.uk>
2013-08-20 11:16:12 +0100

    fixed navbar

--- parts/nav.php
+++ parts/nav.php
@@ -1,15 +1,18 @@
 <nav class="navbar navbar-default" role="nagivation">
   <div class="container">
+  
+	  <div class="navbar-header"> 
+		    <!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
+		    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".main-navigation">
+		      <span class="icon-bar"></span>
+		      <span class="icon-bar"></span>
+		      <span class="icon-bar"></span>
+		    </button>
+		
+		    <? // the brand ?>
+		    <a class="navbar-brand" href="<?= home_url(); ?>"><?= bloginfo('title') ?> </a>
+	  </div><? // navbar-header ?>
 
-    <!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".main-navigation">
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-
-    <? // the brand ?>
-    <a class="navbar-brand" href="<?= home_url(); ?>"><?= bloginfo('title') ?> </a>
 
     <? // Place everything within .navbar-collapse to hide it until above 768px ?>
     <div class="collapse navbar-collapse main-navigation">
@@ -18,7 +21,7 @@
 			$args = array(
 				'theme_location' => 'top-bar',
 				'depth'		 => 2,
-				'menu_class'	 => 'nav navbar-nav pull-right',
+				'menu_class'	 => 'nav navbar-nav navbar-right',
 				'walker'	 => new Monolith_Nav_Walker()
 			);
 		
