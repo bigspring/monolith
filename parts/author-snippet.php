652b9a8c0e5f23f6cd11adfd3798eecf952e9da7
Julio Taylor <julio@bigspring.co.uk>
2013-08-20 09:50:05 +0100

    fixed markup in author snippet

--- parts/author-snippet.php
+++ parts/author-snippet.php
@@ -7,13 +7,13 @@
 
 <?php if ( get_the_author_meta( 'description' ) ) : ?>
 
-	<div itemscope itemtype="http://www.schema.org/Person" class="article-author panel">
+	<div itemscope itemtype="http://www.schema.org/Person" class="article-author panel panel-default">
 				
 		<div class="panel-heading">
 			<h3 class="author-snippet-name panel-title" itemprop="author">About the author</h3>
 		</div>
 		
-		<div class="panel-content">
+		<div class="panel-body">
 			<div class="author-image pull-right" itemprop="image"><?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?></div>
 
 			<h3><?php the_author() ?></h3>
