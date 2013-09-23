87b8d2c04212c4a19f423539c8cffcec57345ec0
Jon Martin <jon@bigspring.co.uk>
2013-09-23 13:51:09 +0100

    Fixes Issue 85
    Implemented a new Wordpress shortcode that renders Bootstrap panels.

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -536,6 +536,24 @@ function columns_shortcode($atts, $content = null) {
 	return '<div class="columns">' . do_shortcode($content) . '</div>';
 }
 
+/**
+ * Renders a bootstrap panel.  Option to provide a type (to affect the panel's class), header content or footer content
+ */
+function panels_shortcode($atts, $content = null) {
+    if(!$content)
+        return false;
+    
+    $type = (array_key_exists('type', $atts) ? 'panel-' . $atts['type'] : 'panel-default');
+    // get the markup
+    $html = '<div class="panel '.$type.'">';
+    $html .= (array_key_exists('header', $atts) ? '<div class="panel-heading">'.$atts['header'].'</div>' : ''); // render a header if we need one   
+    $html .= '<div class="panel-body">' . $content .'</div>'; // render the main body content
+    $html .= (array_key_exists('footer', $atts) ? '<div class="panel-footer">'.$atts['footer'].'</div>' : ''); // render a footer if we need one
+    $html .= '</div>'; 
+    
+    return $html;
+}
+
 
 // load shortcodes
 add_shortcode('icon', 'bootstrap_glyph_icons');
@@ -549,4 +567,5 @@ add_shortcode('blockquote', 'blockquotes');
 add_shortcode('childpages', 'childpages');
 add_shortcode('pages', 'pages_shortcode');
 add_shortcode('columns', 'columns_shortcode');
+add_shortcode('panel', 'panels_shortcode');
 ?>
