654bac94b16b6812863eac491002ce2ba042629e
Simon Miles <simon@bigspring.co.uk>
2013-10-11 17:08:08 +0100

    #83 added code to inc/functions to stop WP linking images to themselves by default

--- inc/functions.php
+++ inc/functions.php
@@ -344,3 +344,15 @@ function the_post_thumbnail_caption() {
     echo '<span>'.$thumbnail_image[0]->post_excerpt.'</span>';
   }
 }
+
+
+// Stop WP from automatically linking images to themselves 
+function wpb_imagelink_setup() {
+	$image_set = get_option( 'image_default_link_type' );
+	
+	if ($image_set !== 'none') {
+		update_option('image_default_link_type', 'none');
+	}
+}
+add_action('admin_init', 'wpb_imagelink_setup', 10);
+
