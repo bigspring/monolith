a9d4d3568a50f95ee320b473df743b5528b4b0af
Julio Taylor <julio@bigspring.co.uk>
2013-11-29 17:19:37 +0000

    refactored page template

--- page.php
+++ page.php
@@ -7,27 +7,16 @@
 ?>
 
 <?php get_header() ?>
-
-<div class="wrapper-main" role="document">
-	
+<div class="wrapper-main" role="document">	
 	<div class="<?= CONTAINER_CLASSES; ?>">
-
-		<? get_template_part('parts/breadcrumb'); // load the breadcrumbs ?>
-		
-		<div class="<?= ROW_CLASSES ?>">
-			
-			<div class="<?= MAIN_SIZE ?>" role="main" itemscope itemtype="http://schema.org/ItemPage">			
-							
-				<? get_template_part('parts/page-header'); // load the page header ?>
-				<? get_template_part('parts/page-content'); // load the page content ?>
-			
-			</div><!-- /MAIN_SIZES -->
-			
-			<? get_template_part('parts/sidebar'); // load the right sidebar ?>
-		
+		<? get_template_part('parts/breadcrumb'); // load the breadcrumbs ?>		
+		<div class="<?= ROW_CLASSES ?>">			
+			<div class="<?= MAIN_SIZE ?>" role="main" itemscope itemtype="http://schema.org/ItemPage">					
+				<? get_template_part('parts/page-header'); // load the page header ?>		
+				<? get_template_part('parts/page-content'); // load the page content ?>			
+			</div><!-- /MAIN_SIZES -->			
+			<? get_template_part('parts/sidebar'); // load the right sidebar ?>		
 		</div><!-- /ROW_CLASSES -->	
-
 	</div><!-- /CONTAINER_CLASSES -->
-
 </div><!-- /wrapper-main -->
 <?php get_footer(); ?>
