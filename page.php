0155517c0936631c303d27f9f86a32d102a86fcf
Julio Taylor <julio@bigspring.co.uk>
2014-02-05 15:23:17 +0000

    refactored page.php to incude full width header

--- page.php
+++ page.php
@@ -10,9 +10,9 @@
 <div class="wrapper-main" role="document">	
 	<div class="<?= CONTAINER_CLASSES; ?>">
 		<? get_template_part('parts/breadcrumb'); // load the breadcrumbs ?>		
+		<? get_template_part('parts/page-header'); // load the page header ?>
 		<div class="<?= ROW_CLASSES ?>">			
-			<div class="<?= MAIN_SIZE ?>" role="main" itemscope itemtype="http://schema.org/ItemPage">					
-				<? get_template_part('parts/page-header'); // load the page header ?>		
+			<div class="<?= MAIN_SIZE ?>" role="main" itemscope itemtype="http://schema.org/ItemPage">	
 				<? get_template_part('parts/page-content'); // load the page content ?>			
 			</div><!-- /MAIN_SIZES -->			
 			<? get_template_part('parts/sidebar'); // load the right sidebar ?>		
