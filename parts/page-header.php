2105dc717dc547a382ddf9bff38c2872af0504fc
Julio Taylor <julio@bigspring.co.uk>
2014-05-06 14:40:13 +0100

    implemented HTML5 markup in page & sub parts

--- parts/page-header.php
+++ parts/page-header.php
@@ -1,10 +1,8 @@
-<?php
-/*
- *	Monolith
- *	Single page header template
- */
-?>
+<? // monolith: default page header part ?>
 
-<header class="page-header">
+<header class="page-header" role="banner">
 	<h1 itemprop="name"><?php the_title(); ?></h1>
+	<?php if ($post->post_excerpt != '') { // show the excerpt if it exists ?>
+	<p class="page-excerpt lead"><?= get_the_excerpt(); ?></p>
+	<? } ?>
 </header>
