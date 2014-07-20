f86b588250a48ba36f412260ddc2d32af57524b4
Julio Taylor <julio@Julios-Mac-mini.local>
2014-07-20 16:26:10 +0100

    fixed a bug with markup in panels shortcode

--- parts/shortcodes/childpages-panels.php
+++ parts/shortcodes/childpages-panels.php
@@ -2,14 +2,18 @@
 
 <section class="childpages childpages-panel">
 	<?php while ( $childpages->have_posts() ) : $childpages->the_post(); // start the loop ?>
-	
-	<div class="panel">
+
+	<div class="panel panel-default">
 	  <div class="panel-heading">
 	    <h3 class="panel-title"><?= get_the_title() ?></h3>
 	  </div>
-	  <?php the_excerpt() ?>
-	  <a href=" <?= get_permalink(get_the_id()) ?>">Read More &rarr;</a>
+		<div class="panel-body">
+	  	<?php the_excerpt() ?>
+		</div>
+	  <div class="panel-footer">
+			<a href=" <?= get_permalink(get_the_id()) ?>">Read More &rarr;</a>
+		</div>
 	</div>
-	
+
 	<? endwhile; // end the loop ?>
 </section>
