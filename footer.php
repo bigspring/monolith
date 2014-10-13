68fd86d39a88c6364a2344f5b8156697b8419cb4
Julio Taylor <julio@bigspring.co.uk>
2014-10-13 17:09:19 +0100

    implement basic index / header / footer

---
+++ footer.php
@@ -0,0 +1,6 @@
+	<footer role="contentinfo">
+		<hr />
+		[site footer goes here]
+	</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en" >
+
+<head>
+  <meta charset="utf-8">
+  <!-- If you delete this meta tag World War Z will become a reality -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php wp_title(); ?></title>
+
+  <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/assets/css/base.css">
+
+  <script src="js/vendor/modernizr.js"></script>
+
+</head>
+<body>
+
+	<header role="banner">
+		[header goes here]
+		<hr />
+	</header>
