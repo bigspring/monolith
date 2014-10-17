730c375253ad47305fb6f3399d3cd21f4af264f3
Julio Taylor <julio@bigspring.co.uk>
2014-10-17 14:41:45 +0100

    added date molecule

---
+++ layouts/molecules/date.php
@@ -0,0 +1,1 @@
+<p class="entry-meta subheader"><?php _e('Posted by', 'monolith'); ?> <span class="author"><?php the_author(); ?></span>, <time class="published" datetime="<?= get_the_date('Y-m-d H:i:sO'); ?>"><?= get_the_date('l jS F Y'); ?></time></p>
