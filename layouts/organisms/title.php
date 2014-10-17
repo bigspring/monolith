f78c4f567d176a4c89aeda2de5376f77d0f06ba4
Julio Taylor <julio@bigspring.co.uk>
2014-10-17 14:53:31 +0100

    created title part

---
+++ layouts/organisms/title.php
@@ -0,0 +1,11 @@
+<?php 
+ // Monolith by BigSpring
+ // Licensed under MIT Open Source
+ // Title loop
+?>
+
+<!-- start the page header -->
+<header class="entry-header">   
+  <h1 class="entry-title"><?php the_title(); ?></h1>
+</header>
+<!-- end the page header -->
