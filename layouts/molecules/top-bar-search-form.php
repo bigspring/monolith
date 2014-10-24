fc96054d2caaebab19663c534bfb79fab544987a
Julio Taylor <julio@bigspring.co.uk>
2014-10-24 20:47:24 +0100

    added a basic navbar search molecule

---
+++ layouts/molecules/top-bar-search-form.php
@@ -0,0 +1,18 @@
+<?php
+  /**
+  * A search form for inclusion in the top bar, based on http://foundation.zurb.com/docs/components/topbar.html#
+  **/
+?>
+
+<ul class="right">
+<li class="has-form">
+  <div class="row collapse">
+    <div class="large-8 small-9 columns">
+      <input type="text" placeholder="<?php _e('Search...','monolith'); ?>">
+    </div>
+    <div class="large-4 small-3 columns">
+      <a href="#" class="button expand">Search</a>
+    </div>
+  </div>
+</li>
+</ul>
