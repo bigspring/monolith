527fa77875d4ae57e4407ba47173cfdca048a16f
Simon Miles <simon@bigspring.co.uk>
2014-10-24 16:45:25 +0100

    created block grid parts

---
+++ layouts/molecules/block-grid-item.php
@@ -0,0 +1,14 @@
+<li>
+  <a href="<?php the_permalink(); ?>">
+    <?php if(has_post_thumbnail()) : ?>
+      <?php the_post_thumbnail(); ?>
+    <?php else : ?> 
+      [insert defualt thumb here]
+    <?php endif; ?>
+  </a>
+  <div class="caption">
+    <h3><?php the_title(); ?></h3>
+    <?php the_excerpt(); ?>
+    <a class="button small" href="<?php the_permalink(); ?>">Read more</a>
+  </div>
+</li>
