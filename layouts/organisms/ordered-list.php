85d15572071ca09df186fcdef843fcc4d494a981
Simon Miles <simon@bigspring.co.uk>
2014-10-31 11:40:58 +0000

    final commenting

--- layouts/organisms/ordered-list.php
+++ layouts/organisms/ordered-list.php
@@ -1,9 +1,21 @@
-<?php if($loop->have_posts()) : ?>
+<?php
+/**
+ * The ordered list organism that calls in the list-item molecule
+ *
+ * @package monolith
+ */
+?>
+
+<?php if($loop->have_posts()) : // if we have results run the loop  ?>
+
+<!-- start ol wrapper -->
     <ol<?= $args['classes'] ?>>
         <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
             <?php include($layouts_path . 'molecules/list-item.php'); ?>
         <?php endwhile; ?>
     </ol>
-<?php else : ?>
+<!-- end ol wrapper -->    
+
+<?php else : // otherwise show the content none organism ?>
     <?php include($layouts_path . 'organisms/content-none.php'); ?>
 <?php endif; ?>
