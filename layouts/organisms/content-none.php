5bd3c1da1697ae49a37a8893394c7fa79ae0f8ae
Simon Miles <simon@bigspring.co.uk>
2014-10-31 11:29:56 +0000

    added new commenting code

--- layouts/organisms/content-none.php
+++ layouts/organisms/content-none.php
@@ -1,6 +1,13 @@
-<?php 
- // Monolith by BigSpring
- // Licensed under MIT Open Source
+<?php
+/**
+ * The content none organism used in all cases where no results are returned from a loop
+ *
+ * @package monolith
+ */
 ?>
 
-<p>Sorry, no results found.</p>
+<!-- start content-none organism -->
+<div data-alert class="alert-box info">
+Sorry, no results found.
+</div>
+<!-- end content-none organism -->
