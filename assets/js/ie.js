1e43d14775ef4119a1f00245dcf803b32d8e67c8
Julio Taylor <julio@bigspring.co.uk>
2014-11-05 15:59:20 +0000

    dist grunt

---
+++ assets/js/ie.js
@@ -0,0 +1,6 @@
+/*! monolith 2.0.0 - 2014-11-05 15:24:15
+* https://github.com/bigspring/monolith
+* Copyright (c) 2014 BigSpring
+* License: MIT
+* Packages: node-sass, grunt, grunt-contrib-concat, grunt-contrib-uglify, grunt-sass, grunt-contrib-cssmin, grunt-contrib-watch, load-grunt-tasks */
+
