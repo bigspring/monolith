7920452b766a23833f1ed22a918b4675ce6e2179
Julio Taylor <julio@bigspring.co.uk>
2014-11-05 15:59:13 +0000

    fixes and closes #164

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,7 +1,7 @@
-/*! monolith 2.0.0 - 2014-10-31 15:47:18
+/*! monolith 2.0.0 - 2014-11-05 15:24:15
 * https://github.com/bigspring/monolith
-* Copyright (c) 2014 BigSpring;* License: MIT
-
+* Copyright (c) 2014 BigSpring
+* License: MIT
 * Packages: node-sass, grunt, grunt-contrib-concat, grunt-contrib-uglify, grunt-sass, grunt-contrib-cssmin, grunt-contrib-watch, load-grunt-tasks */
 
 /*!
