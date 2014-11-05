b1700f5f4118aba420ab51272ed12a17d4b9753c
Julio Taylor <julio@bigspring.co.uk>
2014-11-05 12:49:21 +0000

    fixes and closes #192

--- layouts/molecules/panel.php
+++ layouts/molecules/panel.php
@@ -1,15 +1,19 @@
 <?php
 /**
- * The panel molecule that gets called in by the builder and shortcodes 
+ * The panel builder molecule, displays a single panel – based on http://foundation.zurb.com/docs/components/panels.html
  *
  * @package monolith
  */
 ?>
 
 <!-- start panel -->
-<div class="monolith-panel <? // $snippet_size ?>">
-  <?php the_post_thumbnail(); ?>
+<div class="monolith-panel panel">
+ 
+  <!-- the panel heading -->
   <h2 class="entry-name"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  
+  <!-- the content -->
   <?php the_content(); ?>
+
 </div>
 <!-- end panel -->
