2ea276cfcdab06f6f1026768eaef7b9fea0e26da
korovacow <korova.cow@gmail.com>
2014-11-09 23:40:53 +0000

    fixes #152 excerpt box position

--- inc/hooks.php
+++ inc/hooks.php
@@ -80,21 +80,20 @@ if ( ! function_exists( 'my_register_mce_button' ) ) {
 }
 
 if ( ! function_exists( 'display_environment_on_admin_bar' ) ) {
+
+}
+
+if (ENVIRONMENT === 'development') {
     /**
      * Display notification in Wordpress bar when in development mode
      */
-    function display_environment_on_admin_bar($wp_admin_bar)
-    {
+    add_action('admin_bar_menu', function ($wp_admin_bar) {
         $args = array(
             'id' => 'monolith_env',
             'title' => '<span style="color: #FFFFFF; background-color: #FF0000; padding: 2px 5px; border-radius: 3px; font-size: 0.9em;">DEV MODE</span>',
         );
         $wp_admin_bar->add_node($args);
-    }
-}
-
-if (ENVIRONMENT === 'development') {
-    add_action('admin_bar_menu', 'display_environment_on_admin_bar', 1);
+    }, 1);
 }
 
 /**
@@ -135,22 +134,23 @@ add_action('admin_menu', function () {
 }, 999);
 
 /**
- *
+ * Add custom excerpt box
  */
 add_action('edit_form_after_title', function ($post) {
-    ?>
-    <br>
-    <div id="postbox-container-666" class="postbox-container">
+    if (post_type_supports($post->post_type, 'excerpt')) {
+        ?>
+        <br>
         <div id="normal-sortables" class="meta-box-sortables">
             <div id="postexcerpt" class="postbox">
-                <div class="handlediv" title="Click to toggle"><br /></div>
+                <div class="handlediv" title="Click to toggle"><br/></div>
                 <h3 class='hndle'><span>Excerpt</span></h3>
                 <div class="inside">
                     <label class="screen-reader-text" for="excerpt">Excerpt</label>
                     <textarea rows="1" cols="40" name="excerpt" id="excerpt"><?= $post->post_excerpt ?></textarea>
-                    <p>A brief summary of the full post content.</p>
+                    <p>A brief summary of the <?= $post->post_type ?> content.</p>
                 </div>
+            </div>
         </div>
-    </div>
-    <?php
+        <?php
+    }
 });
