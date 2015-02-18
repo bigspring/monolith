548b36c1fc7b9a43c72bfe3b9a952b26bd2c865d
Simon Miles <simon@bigspring.co.uk>
2015-02-18 15:13:23 +0000

    added rem polyfil

--- header.php
+++ header.php
@@ -17,6 +17,13 @@
         <script src="<?= get_asset_uri('js', 'ie') ?>"></script>
         <![endif]-->
 
+				<!--[if lt IE 9]>
+				  <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
+				  <script src="//s3.amazonaws.com/nwapi/nwmatcher/nwmatcher-1.2.5-min.js"></script>
+				  <script src="//html5base.googlecode.com/svn-history/r38/trunk/js/selectivizr-1.0.3b.js"></script>
+				  <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
+				<![endif]-->
+
     </head>
 
     <body <?php body_class(); ?>>
