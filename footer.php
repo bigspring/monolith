d431f98c353b4e9da4ca6bcf71dba107cf7a9652
hibernius <korova.cow@gmail.com>
2015-10-09 23:51:03 +0100

    WP Standards/tidy

--- footer.php
+++ footer.php
@@ -1,32 +1,32 @@
-              </div><!-- end the main structural div (.columns) -->
+        </div><!-- end the main structural div (.columns) -->
 
-              <!-- start the sidebar -->
-              <?php if( !is_page_template('page-fullwidth.php') ) : // do not load sidebar if using fullwidth template ?>
-              <div id="sidebar" class="columns <?= SIDEBAR_SIZE; ?> sidebar" role="complementary">
-                <?php get_template_part('layouts/organisms/sidebar'); ?>
-              </div>
-              <?php endif; ?>
-              <!-- end the sidebar -->
-
-            </div><!-- /end the .row -->
-
-        </main>
-
-        <!-- the site footer -->
-        <?php get_template_part('layouts/organisms/site-footer'); ?>
-
-        <?php if (ENVIRONMENT === 'development') : ?>
-            <!-- dev specific stuff -->
-            <div class="debug"></div>
-            <script src="//localhost:35729/livereload.js"></script>
+        <!-- start the sidebar -->
+        <?php if ( ! is_page_template( 'page-fullwidth.php' ) ) : // do not load sidebar if using fullwidth template ?>
+          <div id="sidebar" class="columns <?= SIDEBAR_SIZE; ?> sidebar" role="complementary">
+            <?php get_template_part( 'layouts/organisms/sidebar' ); ?>
+          </div>
         <?php endif; ?>
-        <script src="<?= get_asset_uri('js', 'base') ?>"></script>
-        <?php wp_footer(); ?>
+        <!-- end the sidebar -->
 
-				<?php /* load the fail rem code for ie only */ ?>
-				<!--[if lt IE 9]>
-				<script src="<?= get_asset_uri('js', 'ie8') ?>" type="text/javascript"></script>
-				<![endif]-->
+      </div><!-- /end the .row -->
 
-    </body>
+    </main>
+
+    <!-- the site footer -->
+    <?php get_template_part( 'layouts/organisms/site-footer' ); ?>
+
+    <?php if ( ENVIRONMENT === 'development' ) : ?>
+      <!-- dev specific stuff -->
+      <div class="debug"></div>
+      <script src="//localhost:35729/livereload.js"></script>
+    <?php endif; ?>
+    <script src="<?= get_asset_uri( 'js', 'base' ) ?>"></script>
+    <?php wp_footer(); ?>
+
+    <?php /* load the fail rem code for ie only */ ?>
+    <!--[if lt IE 9]>
+    <script src="<?= get_asset_uri('js', 'ie8') ?>" type="text/javascript"></script>
+    <![endif]-->
+
+  </body>
 </html>
--- header.php
+++ header.php
@@ -1,66 +1,62 @@
 <!DOCTYPE html>
-<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
-<html class="no-js" lang="en" >
+<!--[if IE 9]><html class="lt-ie10" lang="en"> <![endif]-->
+<html class="no-js" lang="en">
 
-    <head>
-        <meta charset="utf-8">
-        <!-- If you delete this meta tag World War Z will become a reality -->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php wp_title(); ?></title>
+  <head>
+    <meta charset="utf-8">
+    <!-- If you delete this meta tag World War Z will become a reality -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title(); ?></title>
 
-		    <!-- Fav and touch icons -->
-		    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/assets/icons/touch-icon-ipad-retina.png">
-		    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/assets/icons/touch-icon-iphone-retina.png">
-		    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/assets/icons/touch-icon-ipad.png">
-		    <link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/assets/icons/touch-icon-iphone.png">
-		    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/icons/favicon.ico">
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144"
+          href="<?php echo get_template_directory_uri(); ?>/assets/icons/touch-icon-ipad-retina.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114"
+          href="<?php echo get_template_directory_uri(); ?>/assets/icons/touch-icon-iphone-retina.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72"
+          href="<?php echo get_template_directory_uri(); ?>/assets/icons/touch-icon-ipad.png">
+    <link rel="apple-touch-icon-precomposed"
+          href="<?php echo get_template_directory_uri(); ?>/assets/icons/touch-icon-iphone.png">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/icons/favicon.ico">
 
 
-        <?php wp_head(); ?>
+    <?php wp_head(); ?>
 
-        <link rel="stylesheet" href="<?= get_asset_uri('css', 'base') ?>">
-        <!-- HTML5 & responsive support for IE browsers... -->
-        <!--[if lte IE 9]>
-        <link href="<?= get_asset_uri('css', 'ie') ?>" rel="stylesheet" type="text/css">
-        <script src="<?= get_asset_uri('js', 'ie') ?>"></script>
-        <![endif]-->
+    <link rel="stylesheet" href="<?= get_asset_uri( 'css', 'base' ) ?>">
+    <!-- HTML5 & responsive support for IE browsers... -->
+    <!--[if lte IE 9]>
+    <link href="<?= get_asset_uri('css', 'ie') ?>" rel="stylesheet" type="text/css">
+    <script src="<?= get_asset_uri('js', 'ie') ?>"></script>
+    <![endif]-->
 
-				<!--[if lt IE 9]>
-			  <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
-			  <script src="//s3.amazonaws.com/nwapi/nwmatcher/nwmatcher-1.2.5-min.js"></script>
-			  <script src="//html5base.googlecode.com/svn-history/r38/trunk/js/selectivizr-1.0.3b.js"></script>
-			  <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
-				<![endif]-->
+    <!--[if lt IE 9]>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
+    <script src="//s3.amazonaws.com/nwapi/nwmatcher/nwmatcher-1.2.5-min.js"></script>
+    <script src="//html5base.googlecode.com/svn-history/r38/trunk/js/selectivizr-1.0.3b.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
+    <![endif]-->
 
-    </head>
+  </head>
 
-    <body <?php body_class(); ?>>
+  <body <?php body_class(); ?>>
 
-        <?php get_template_part('layouts/organisms/nav-topbar'); // load the navigation ?>
+    <?php get_template_part( 'layouts/organisms/nav-topbar' ); // load the navigation ?>
 
-        <!-- the main block -->
-        <main class="block-main" role="main">
+    <!-- the main block -->
+    <main class="block-main" role="main">
 
-          <!-- the breadcrumbs block -->
-          <?php get_template_part('layouts/molecules/breadcrumbs'); ?>
+      <!-- the breadcrumbs block -->
+      <?php get_template_part( 'layouts/molecules/breadcrumbs' ); ?>
 
-          <!-- page title & hero unit -->
-          <?php
+      <!-- page title & hero unit -->
+      <?php if ( is_front_page() ) : // if it's a static homepage, load the hero unit ?>
+        <?php get_template_part( 'layouts/organisms/hero-unit' ); ?>
+      <?php else : // otherwise, load the standard page header ?>
+        <?php get_template_part( 'layouts/organisms/page-header' ); ?>
+      <?php endif; ?>
 
-            if ( is_front_page() ) : // if it's a static homepage, load the hero unit
+      <!-- start the main content row -->
+      <div class="row">
 
-              get_template_part('layouts/organisms/hero-unit');
-
-            else : // otherwise, load the standard page header
-
-              get_template_part('layouts/organisms/page-header');
-
-            endif;
-
-          ?>
-
-          <!-- start the main content row -->
-          <div class="row">
-
-            <?php // if we're using the fullwdith template, apply the relevant class ?>
-            <div class="columns <?= is_page_template('page-fullwidth.php') ? FULLWIDTH_SIZE : MAIN_SIZE; ?>" role="main">
+        <?php // if we're using the fullwdith template, apply the relevant class ?>
+        <div class="columns <?= is_page_template( 'page-fullwidth.php' ) ? FULLWIDTH_SIZE : MAIN_SIZE; ?>" role="main">
