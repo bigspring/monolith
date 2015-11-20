d896b3c8fd1e52b53f7d27c59fed6184b80edee5
Dave Seaton <dave@bigspring.co.uk>
2015-11-20 15:24:27 +0000

    Implement livereload

--- footer.php
+++ footer.php
@@ -1,23 +1,24 @@
-          </div><!-- end the main structural div (.columns) -->
+            </div><!-- end the main structural div (.columns) -->
 
-          <!-- start the sidebar -->
-          <?php if( !is_page_template('page-fullwidth.php') ) : // do not load sidebar if using fullwidth template ?>
-          <div id="sidebar" class="columns <?= SIDEBAR_SIZE; ?> sidebar" role="complementary">
-            <?php get_template_part('layouts/organisms/sidebar'); ?>
-          </div>
-          <?php endif; ?>
-          <!-- end the sidebar -->
+            <!-- start the sidebar -->
+            <?php if( !is_page_template('page-fullwidth.php') ) : // do not load sidebar if using fullwidth template ?>
+            <div id="sidebar" class="columns <?= SIDEBAR_SIZE; ?> sidebar" role="complementary">
+              <?php get_template_part('layouts/organisms/sidebar'); ?>
+            </div>
+            <?php endif; ?>
+            <!-- end the sidebar -->
 
-        </div><!-- /end the .row -->
+          </div><!-- /end the .row -->
+
+        </main>
 
         <!-- the site footer -->
         <?php get_template_part('layouts/organisms/site-footer'); ?>
 
         <?php if (ENVIRONMENT === 'development') : ?>
             <!-- debug stuff -->
-            <div class="debug">
-                <!-- use this section to test out features in development mode -->
-            </div>
+          <div class="debug"></div>
+          <script src="//localhost:35729/livereload.js"></script>
         <?php endif; ?>
         <script src="<?= get_asset_uri('js', 'base') ?>"></script>
         <?php wp_footer(); ?>
