a5738cf304d218681464baddcdd6bd2b375a4759
simonpmiles <simon@bigspring.co.uk>
2015-12-29 11:56:11 +0000

    Language tag on 404 page
    Added the _e function to some bits of text that weren't currently translatable

--- 404.php
+++ 404.php
@@ -9,19 +9,19 @@ get_header(); ?>
 <div class="row">
   <div class="columns medium-8 medium-centered">
 
-  <div class="error-404">
-
-    <header class="text-center">
-      <h1><?php _e('Page not found (error 404)','monolith'); ?></h1>
-      <p class="lead"><?php _e('Sorry – the page you were looking for does not exist.','monolith'); ?></p>
-      <p>Try going <a href="<?php bloginfo('url'); ?>">back to the homepage</a>, or search the site using the form below:</p>
-
-      <?php get_template_part('layouts/molecules/searchform'); ?>
-
-    </header>
-  </div>
+	  <div class="error-404">
+	
+	    <header class="text-center">
+	      <h1><?php _e('Page not found (error 404)','monolith'); ?></h1>
+	      <p class="lead"><?php _e('Sorry – the page you were looking for does not exist.','monolith'); ?></p>
+	      <p><?php _e('Try going','monolith'); ?> <a href="<?php bloginfo('url'); ?>"><?php _e('back to the homepage,','monolith'); ?></a> <?php _e('or search the site using the form below:','monolith'); ?></p>
+	
+	      <?php get_template_part('layouts/molecules/searchform'); ?>
+	
+	    </header>
+	  </div>
   
-</div>
+	</div>
 </div>
 
 
