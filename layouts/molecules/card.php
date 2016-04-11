32d1c6b740680465ef3a374250261d40e991896f
Dave Seaton <dave@bigspring.co.uk>
2016-04-11 14:03:49 +0100

    resolve #385 change anchor attribute in card, strip spaces

--- layouts/molecules/card.php
+++ layouts/molecules/card.php
@@ -7,43 +7,43 @@
 
 <!-- start block grid li -->
 <li class="item">
-  <?php if ( $args['has_link'] ) : ?>
-  <a name="<?= the_title(); ?>" href="<?php the_permalink(); ?>">
-    <?php endif; ?>
+	<?php if ( $args['has_link'] ) : ?>
+	<a id="<?= sanitize_title( get_the_title() ) ?>" href="<?php the_permalink(); ?>">
+		<?php endif; ?>
 
-    <div class="card">
-      <?php if ( $args['has_image'] ) : // display the featured image if argument is true ?>
-        <div class="featured-image">
-          <?php the_monolith_post_thumbnail( 'large' ); ?>
-        </div>
-      <?php endif; // end has_image ?>
+		<div class="card">
+			<?php if ( $args['has_image'] ) : // display the featured image if argument is true ?>
+				<div class="featured-image">
+					<?php the_monolith_post_thumbnail( 'large' ); ?>
+				</div>
+			<?php endif; // end has_image ?>
 
-      <div class="caption" data-equalizer-watch>
-        <?php if ( $args['has_title'] ) : // display only if the summary is enabled (default is true) ?>
-          <h3 class="title <?= $args['has_summary'] ? 'has-summary' : '' ?>"><?php the_title(); ?></h3>
-        <?php endif; // end has_title ?>
+			<div class="caption" data-equalizer-watch>
+				<?php if ( $args['has_title'] ) : // display only if the summary is enabled (default is true) ?>
+					<h3 class="title <?= $args['has_summary'] ? 'has-summary' : '' ?>"><?php the_title(); ?></h3>
+				<?php endif; // end has_title ?>
 
-        <?php if ( $args['has_summary'] ) : // display only if the summary is enabled (default is true) ?>
-          <div class="summary">
+				<?php if ( $args['has_summary'] ) : // display only if the summary is enabled (default is true) ?>
+					<div class="summary">
 
-            <?= wp_trim_words( get_the_excerpt(), 35, '...' ); ?>
+						<?= wp_trim_words( get_the_excerpt(), 35, '...' ); ?>
 
-          </div>
-        <?php endif; // end has_summary ?>
+					</div>
+				<?php endif; // end has_summary ?>
 
 
-      </div>
+			</div>
 
-      <?php if ( $args['has_link'] && $args['has_readmore'] ) : // display only if the summary is enabled (default is true) ?>
-        <div class="read-more">
-          <?php _e( 'Read more &rarr;', 'monolith' ); ?>
-        </div>
-      <?php endif; // end ?>
+			<?php if ( $args['has_link'] && $args['has_readmore'] ) : // display only if the summary is enabled (default is true) ?>
+				<div class="read-more">
+					<?php _e( 'Read more &rarr;', 'monolith' ); ?>
+				</div>
+			<?php endif; // end ?>
 
-    </div>
+		</div>
 
-    <?php if ( $args['has_link'] ) : ?>
-  </a>
+		<?php if ( $args['has_link'] ) : ?>
+	</a>
 <?php endif; ?>
 
 </li>
